d898876c824baa8f9be2e5bc70b6b5c3a67a91c6
jadoucis <jade.grau@epsi.fr>
2022-12-02 10:25:07 +0100

    add material module

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+/* Material */
+import {LayoutModule} from "@angular/cdk/layout";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+
+@NgModule({
+  exports:[
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule
+  ]
+})
+export class AppMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.modules';
 import { LdapListComponent } from './ldap-list/ldap-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppMaterialModule } from './app-material.module';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,10 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
