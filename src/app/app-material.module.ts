82775af0cbaf479b6ef90f44865c14fc4aa91d68
jadoucis <jade.grau@epsi.fr>
2022-12-02 11:53:40 +0100

    add navbar

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 /* Material */
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {LayoutModule} from "@angular/cdk/layout";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
@@ -13,6 +14,7 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
   exports:[
+    BrowserAnimationsModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,31 @@ import {AppRoutingModule} from './app-routing.modules';
 import { LdapListComponent } from './ldap-list/ldap-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppMaterialModule } from './app-material.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     LdapListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
