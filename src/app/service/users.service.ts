d10908814237cbbb5feef43c23727cad4269f660
jadoucis <jade.grau@epsi.fr>
2023-01-18 13:14:30 +0100

    add service and users

---
+++ src/app/service/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {UserLdap} from "../model/user-ldap";
+import {LDAP_USERS} from "../model/ldap-mock-data";
+import {Observable, of} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  // Users List
+  users: UserLdap[] = LDAP_USERS;
+  constructor() { }
+
+  getUsers(login: string): Observable<UserLdap> {
+    return of(this.users.find(user => user.login === login));
+  }
+}
